a3d12e7eddc6c5057e90bdf998fbb55b1d6d74f7
phamthu <phamthu@192.168.231.1>
2020-04-12 11:33:35 +0700

    V1.1

--- src/BaiTap1/BaiTap1Main.java
+++ src/BaiTap1/BaiTap1Main.java
@@ -16,6 +16,19 @@ public class BaiTap1Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        NhanVien nhanVien = new NhanVien("Phạm Thị Thu", "20", "Nha Trang", 400, 4000);
+        System.out.println("Tên:"+ nhanVien.getTen());
+        System.out.println("Tuổi: "+nhanVien.getTuoi());
+        System.out.println("Địa Chỉ:"+nhanVien.getDiaChi());
+        System.out.println("Tổng số giờ làm:"+nhanVien.getTongSoGioLam());
+        System.out.println("Tiền Lương :"+nhanVien.getTienluong());
+       
+
+        System.out.println("Thông tin nhân viên:\n"+nhanVien.getThongTin());
+        
+        
+        System.out.println("Tổng lương của nhân viên: "+ nhanVien.tinhThuong());
+        
     }
     
 }
